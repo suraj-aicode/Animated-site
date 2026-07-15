457c3092396bc702a11a8b5d842cf76f4580b95e
suraj <suraj40praj@gmail.com>
2026-07-15 20:50:38 +0530

    Some changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { useGSAP } from "@gsap/react";
 import NutritionSection from "./sections/NutritionSection";
 import BenefitSection from "./sections/BenefitSection";
 import TestimonialSection from "./sections/TestimonialSection";
+import FooterSection from "./sections/FooterSection";
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
@@ -35,7 +36,8 @@ const App = () => {
             <BenefitSection />
             <TestimonialSection />
           </div>
-          <div className="h-dvh border border-red-500"></div>
+
+          <FooterSection />
         </div>
       </div>
     </>
